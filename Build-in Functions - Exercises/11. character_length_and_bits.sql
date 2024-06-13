dcd142b99693041d1bae2963f1753443f692f486
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-06-13 10:25:39 +0300

    Added solution

---
+++ Build-in Functions - Exercises/11. character_length_and_bits.sql
@@ -0,0 +1,20 @@
+SELECT
+    CONCAT(mountain_range, ' ', peak_name) AS mountain_information,
+    LENGTH(CONCAT(mountain_range, ' ', peak_name)) AS characters_length,
+    BIT_LENGTH(CONCAT(mountain_range, ' ', peak_name)) AS bits_of_a_tring
+FROM mountains, peaks
+WHERE mountains.id= peaks.mountain_id
+;
+
+
+
+SELECT
+	CONCAT(m.mountain_range, ' ', p.peak_name) AS mountain_information,
+	CHAR_LENGTH(CONCAT(m.mountain_range, ' ', p.peak_name)) AS characters_length,
+	BIT_LENGTH(CONCAT(m.mountain_range, ' ', p.peak_name)) AS bits_of_a_string
+	
+FROM 
+	mountains AS m,
+	peaks AS p
+WHERE
+	m."id" = p.mountain_id;
