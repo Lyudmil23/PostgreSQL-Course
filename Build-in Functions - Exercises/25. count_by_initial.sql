b030e0b5d1b8adbb91689fbc193bfac1e4788a0c
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-06-17 23:22:27 +0300

    Added files

---
+++ Build-in Functions - Exercises/25. count_by_initial.sql
@@ -0,0 +1,18 @@
+SELECT
+	LEFT(first_name, 2) AS initials,
+	COUNT('initials') AS user_count
+FROM users
+GROUP BY initials
+ORDER BY 
+	user_count DESC,
+	initials
+;
+
+
+SELECT
+    LEFT(first_name, 2) AS initials,
+    COUNT(LEFT(first_name, 2)) AS user_count
+FROM users
+GROUP BY initials
+ORDER BY user_count DESC, initials
+;
