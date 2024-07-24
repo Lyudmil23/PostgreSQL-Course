b211083f8ad908cb75654a9380a75ae96545a247
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-07-24 19:19:44 +0300

    Added solution

---
+++ Exam Preparation 1/3.7 owners_and_their_animals.sql
@@ -0,0 +1,10 @@
+SELECT
+    o.name AS owner,
+    COUNT(a.owner_id) AS count_of_animals
+FROM owners AS o
+JOIN animals AS a
+ON o.id = a.owner_id
+GROUP BY owner
+ORDER BY count_of_animals DESC, owner
+LIMIT 5
+;
