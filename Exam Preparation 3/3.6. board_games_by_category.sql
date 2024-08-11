f79d8d083b161f7c62a7587289ffe3ef7576baa7
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-08-11 15:59:55 +0300

    Added solution

---
+++ Exam Preparation 3/3.6. board_games_by_category.sql
@@ -0,0 +1,25 @@
+-- 1st 
+SELECT
+    bg.id,
+    bg.name,
+    bg.release_year,
+    c.name AS category_name
+FROM board_games AS bg
+JOIN categories AS c
+ON bg.category_id = c.id
+WHERE c.name = 'Strategy Games' OR c.name = 'Wargames'
+ORDER BY bg.release_year DESC
+;
+
+-- 2nd 
+SELECT
+    bg.id,
+    bg.name,
+    bg.release_year,
+    c.name AS category_name
+FROM board_games AS bg
+JOIN categories AS c
+ON bg.category_id = c.id
+WHERE c.name IN ('Strategy Games', 'Wargames')
+ORDER BY bg.release_year DESC
+;
