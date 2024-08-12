995dbe12c950e1b7852eb7a9a97f563905c99be0
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-08-12 16:07:30 +0300

    Added solutions

---
+++ Exam Preparation 3/3.9. creators_with_emails.sql
@@ -0,0 +1,26 @@
+-- 1st 
+SELECT 
+    c.first_name || ' ' || c.last_name AS full_name,
+    c.email,
+    MAX(bg.rating) AS rating
+FROM creators c
+JOIN creators_board_games cbg ON c.id = cbg.creator_id
+JOIN board_games bg ON cbg.board_game_id = bg.id
+WHERE c.email LIKE '%.com'
+GROUP BY c.id
+ORDER BY full_name ASC;
+
+
+-- 2nd 
+SELECT
+    CONCAT(c.first_name, ' ', c.last_name) AS full_name,
+    c.email,
+    MAX(bg.rating) AS rating
+FROM creators AS c
+JOIN creators_board_games AS cbg
+ON c.id = cbg.creator_id
+JOIN board_games AS bg
+ON cbg.board_game_id = bg.id
+WHERE c.email LIKE '%.com'
+GROUP BY full_name, c.email
+ORDER BY full_name
