0a43f9fcce4470576da6271f7022c0d74bf4c2dd
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-08-12 16:07:16 +0300

    Added solutions

---
+++ Exam Preparation 3/3.8. first_5_board_games.sql
@@ -0,0 +1,46 @@
+-- first 
+SELECT
+    bg.name,
+    bg.rating,
+    c.name AS category_name
+FROM board_games AS bg
+LEFT JOIN categories AS c
+ON bg.category_id = c.id
+LEFT JOIN players_ranges AS pr
+ON bg.players_range_id = pr.id
+WHERE (bg.rating > 7 AND bg.name ILIKE '%a%')
+    OR (bg.rating > 7.50 AND pr.min_players >= 2 AND pr.max_players <= 5)
+ORDER BY bg.name, bg.rating DESC
+LIMIT 5
+;
+
+-- second 
+SELECT
+    bg.name,
+    bg.rating,
+    c.name AS category_name
+FROM board_games AS bg
+JOIN categories AS c
+ON bg.category_id = c.id
+JOIN players_ranges AS pr
+ON bg.players_range_id = pr.id
+WHERE (bg.rating > 7 AND bg.name ILIKE '%a%')
+    OR (bg.rating > 7.50 AND pr.min_players >= 2 AND pr.max_players <= 5)
+ORDER BY bg.name, bg.rating DESC
+LIMIT 5
+;
+
+-- third 
+SELECT
+    bg.name AS name,
+    bg.rating,
+    c.name AS category_name
+FROM board_games AS bg
+JOIN players_ranges AS pr
+ON bg.players_range_id = pr.id
+JOIN categories AS c
+ON bg.category_id = c.id
+WHERE bg.rating > 7 AND ((bg.name ILIKE '%a%') OR (bg.rating > 7.5 AND pr.min_players >= 2 and pr.max_players <= 5))
+ORDER BY bg.name, bg.rating DESC 
+LIMIT 5
+;
