27f7d4c9dfeffd77ee87af7f5c45a2176f687101
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-08-25 17:42:46 +0300

    Added solutions

---
+++ PostgreSQL Retake Exam - 04 December 2023/2.4. delete.sql
@@ -0,0 +1,26 @@
+-- 1st 
+DELETE FROM products_ingredients
+WHERE ingredient_id IN (
+    SELECT id
+    FROM ingredients
+    WHERE distributor_id IN (
+        SELECT id
+        FROM distributors
+        WHERE name LIKE 'L%'
+    )
+);
+
+DELETE FROM ingredients
+WHERE distributor_id IN (
+    SELECT id
+    FROM distributors
+    WHERE name LIKE 'L%'
+);
+
+DELETE FROM distributors
+WHERE name LIKE 'L%'
+
+-- 2nd 
+DELETE
+FROM distributors
+WHERE name LIKE 'L%';
